9a02dda1e10f4643b8664df268ffec24c7ea09f3
미니 <kminh940@naver.com>
2021-11-03 22:55:47 +0900

    출력

---
+++ 10171. 고양이/10171. 고양이.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//10171. °í¾çÀÌ
+int main()
+{
+	cout << "\\    /\\\n";
+	cout << " )  ( ')\n";
+	cout << "(  /  )\n";
+	cout << " \\(__)|";
+
+	return 0;
+}
