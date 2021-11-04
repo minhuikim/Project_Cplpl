1fe677e82a93417cfe234e33063a9ea0753e115e
미니 <kminh940@naver.com>
2021-11-04 21:45:41 +0900

    출력

---
+++ 10172. 개/10172. 개.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// 10172. °³
+int main()
+{
+	string str;
+	str = "|\\_/|\n";
+	str += "|q p|   /}\n";
+	str += "( 0 )\"\"\"\\\n";
+	str += "|\"^\"`    |\n";
+	str += "||_/=\\\\__|";
+
+	cout << str << endl;
+
+	return 0;
+}
