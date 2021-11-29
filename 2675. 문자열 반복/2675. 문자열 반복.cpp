abed5bed7ec70582ec01ff24485eda1ad8b1d1c4
미니 <kminh940@naver.com>
2021-11-29 21:36:08 +0900

    2675. 문자열 반복

---
+++ 2675. 문자열 반복/2675. 문자열 반복.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string S, P;
+	int T, R;
+
+	cin >> T;
+
+	if (T > 1000) T = 1000;
+
+	for (int i = 0; i < T; i++)
+	{
+		cin >> R;
+		if (R > 8) R = 8;
+		cout << " ";
+		cin >> S;
+
+		for (int j = 0; j < S.length(); j++)
+		{
+			for(int k = 0; k < R; k++)
+			{
+				P += S[j];
+			}
+		}
+		P += "\n";
+	}
+
+	cout << P;
+
+	return 0;
+}
