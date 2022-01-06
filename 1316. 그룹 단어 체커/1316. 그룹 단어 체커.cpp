8f6bd9941b0a1183f3b4d5a64c5aeeccf1e66664
미니 <kminh940@naver.com>
2022-01-06 20:44:01 +0900

    1316. 그룹 단어 체커

--- 1316. 그룹 단어 체커/1316. 그룹 단어 체커.cpp
+++ 1316. 그룹 단어 체커/1316. 그룹 단어 체커.cpp
@@ -4,46 +4,33 @@ using namespace std;
 
 int main()
 {
-	int n, cnt = 0;
-	int alpha[26] = { 0, };
+	char text[101];
+	int n, cnt;
 
 	scanf("%d", &n);
 
+	cnt = n;
+
 	for (int i = 0; i < n; i++)
 	{
-		char text[101] {};
-		int k = 1;
+		char a = '0';
 		int alpha[26]{ 0, };
-		bool check = true, check_2 = true;
 
 		scanf("%s", text);
-		printf("%s\n", text);
 
 		for (int j = 0; text[j] != '\0'; j++)
 		{
-			if(text[j] != text[j - 1])
+			if (a != text[j])
 			{
-				alpha[text[j] - 97] = 1;
-				check = false;
+				a = text[j];
+				alpha[text[j] - 'a']++;
 			}
-			
-			if (alpha[text[j] - 97] == 1)
-			{
-				cout << "[2]" << " / ";
-				if (check == false)
-				{
-					cout << "[3]" << " / ";
-					check_2 == false;
-					continue;
-				}
-			}
-			k++;
-			cout << k << "\n";
-		}
 
-		if (check_2 == true)
-		{
-			cnt++;
+			if (alpha[text[j] - 'a'] == 2)
+			{
+				cnt -= 1;
+				break;
+			}
 		}
 	}
 
