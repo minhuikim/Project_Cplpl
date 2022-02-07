084a53333e5b90fe89925c966ee129df7a18c6f4
미니 <kminh940@naver.com>
2022-02-07 22:58:34 +0900

    10926.??!

---
+++ 10926.joonas/10926.joonas.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char name[50];
+
+	scanf("%s", name);
+
+	printf("%s??!", name);
+/*
+	string name;
+
+	cin >> name;
+
+	cout << name << "??!" << endl;
+
+	return 0;
+*/
+}
