33d9e458d83200e7812fc8e7f8e6cbc3510e6127
미니 <kminh940@naver.com>
2022-02-08 23:29:56 +0900

    10828. 스택

---
+++ 10828.스택/10828.스택.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void push(const int x)
+{
+
+}
+
+int pop()
+{
+
+}
+
+int size()
+{
+
+}
+
+int empty()
+{
+
+}
+
+int top()
+{
+
+}
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	int* stack = new int[n];
+
+
+}
