14c431e9c00fb35fb57828cc16719b5ab90b6282
미니 <kminh940@naver.com>
2022-02-24 22:39:04 +0900

    2751.수정렬하기2(vector,sort)

--- 2751.수정렬하기2/2751.수정렬하기2.cpp
+++ 2751.수정렬하기2/2751.수정렬하기2.cpp
@@ -1,41 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	int n, z, idx, max;
-
-	cin >> n;
-
-	int* arr = new int[n];
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		max = idx = 0;
-		for (int j = i; j < n; j++)
-		{
-			if (max < arr[j])
-			{
-				max = arr[j];
-				idx = j;
-			}
-		}
-		z = arr[i];
-		arr[i] = arr[idx];
-		arr[idx] = z;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << endl;
-	}
-	
-	return 0;
+int main() {
+    int num, tmp;
+    vector<int> a;
+    cin >> num;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> tmp;
+        a.push_back(tmp);
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < num; i++)
+        cout << a[i] << '\n';
 }
