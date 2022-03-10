bb2bbc51feee3bcb8677de0bfb5af4ffcd2ffc83
미니 <kminh940@naver.com>
2022-03-11 00:13:17 +0900

    10250.ACM호텔

--- 10250.ACM호텔/10250.ACM호텔.cpp
+++ 10250.ACM호텔/10250.ACM호텔.cpp
@@ -3,24 +3,35 @@ using namespace std;
 
 int main()
 {
-	int t, h, w, n, height = 0, width = 0, room;
+	int t, height = 0, width = 0;
 
 	cin >> t;
 
+	int* h = new int[t];
+	int* w = new int[t];
+	int* n = new int[t];
+
 	for (int i = 0; i < t; i++)
 	{
-		cin >> h >> w >> n;
-
-		for (int j = 0; j < n; j++)
-		{
-			height = (n - (h * (n / h))) * 100;
-
-			width = (n / h) + 1;
-		}
-
-		cout << (height + width) << endl;
+		cin >> h[i] >> w[i] >> n[i];
 	}
 
+	for (int i = 0; i < t; i++)
+	{
+		height = (n[i] - (h[i] * (n[i] / h[i]))) * 100;
+		width = (n[i] / h[i]) + 1;
+
+		if (!height)
+		{
+			height = h[i] * 100;
+			width = (n[i] / h[i]);
+		}
+		if (!width) width = n[i];
+
+		cout << (height + width) << "\n";
+	}
+
+	delete[] h, w, n;
 	
 	return 0;
 }
