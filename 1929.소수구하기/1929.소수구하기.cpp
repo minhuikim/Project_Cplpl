fbad8f0d056c34bd02e148950b5afa185873c0e5
미니 <kminh940@naver.com>
2022-05-02 23:21:08 +0900

    1929. 소수구하기

--- 1929.소수구하기/1929.소수구하기.cpp
+++ 1929.소수구하기/1929.소수구하기.cpp
@@ -1,31 +1,27 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-using namespace std;
-
-int arr[1000001] = { 0 };
 
 int main()
 {
-	//ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int m, n, cnt = 0;
+	char num[1000001] = { 0 };
 
-	int m, n;
+	scanf("%d%d", &m, &n);
 
-	cin >> m >> n;
-	arr[1] = 1;
-
+	num[1] = 1;
 	for (int i = 2; i <= n; i++)
 	{
-		for (int j = 2; j <= n; j++)
+		for (int j = 2; i * j <= n; j++)
 		{
-			arr[i * j] = 1;
+			num[i*j] = 1;
 		}
 	}
 
 	for (int i = m; i <= n; i++)
 	{
-		if (arr[i] == 0)
+		if (num[i] == 0)
 		{
-			cout << i << endl;
+			printf("%d\n", i);
 		}
 	}
 	
