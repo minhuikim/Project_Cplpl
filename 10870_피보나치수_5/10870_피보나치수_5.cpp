e46a1a59e69412ac48272a8431c3eff7d5a8dbe9
미니 <kminh940@naver.com>
2022-05-17 00:50:43 +0900

    10870_피보나치수_5

--- 10870_피보나치수_5/10870_피보나치수_5.cpp
+++ 10870_피보나치수_5/10870_피보나치수_5.cpp
@@ -1,37 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int fivonacci(int n, int cnt, int fivo[])
+//int fibonacci(int n, int cnt, int fibo[])
+int fibonacci(int n)
 {
 	// Fn = Fn-1 + Fn-2
 	// F[cnt] = F[cnt-1] + F[cnt-2]
 
-	fivo[0] = 0;
+	if (n < 2)
+		return n;
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
+
+	/*
+	fibo[0] = 0;
 
 	if (n > 0 && cnt < 1)
 	{
 		cnt++;
-		fivo[cnt] = cnt;
-		return fivonacci(n - 1, cnt, fivo);
+		fibo[cnt] = cnt;
+		return fibonacci(n - 1, cnt, fibo);
 	}
 	else if (n > 0)
 	{
 		cnt++;
-		fivo[cnt] = fivo[cnt - 1] + fivo[cnt - 2];
-		return fivonacci(n - 1, cnt, fivo);
+		fibo[cnt] = fibo[cnt - 1] + fibo[cnt - 2];
+		return fibonacci(n - 1, cnt, fibo);
 	}
 	else
-		return fivo[cnt];
+		return fibo[cnt];
+	*/
 	
 }
 
 int main()
 {
-	int n, arr[20] = { 0 };
+	int n;
 
 	scanf("%d", &n);
 
-	printf("%d", fivonacci(n, 0, arr));
+	printf("%d", fibonacci(n));
+
+	//printf("%d", fibonacci(n, 0, arr));
 
 	return 0;
 }
