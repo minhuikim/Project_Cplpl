6c17cbd8035edd7b707f149c0dba862c5a2a5deb
미니 <kminh940@naver.com>
2022-05-18 22:31:40 +0900

    17478_재귀함수가_뭔가요?

--- 17478_재귀함수가_뭔가요/17478_재귀함수가_뭔가요.cpp
+++ 17478_재귀함수가_뭔가요/17478_재귀함수가_뭔가요.cpp
@@ -4,33 +4,24 @@
 #include <string>
 using namespace std;
 
-string jh_say(int n, string s)
+void jh_say(int n, string s)
 {	
+	cout << s << "\"재귀함수가 뭔가요?\"\n";
+
 	if (n == 0)
 	{
-		cout << s << "\"재귀함수가 뭔가요?\"\n";
 		cout << s << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
 		cout << s << "라고 답변하였지.\n";
-		return jh_say(n - 1, s.erase(0, 4));
+		return;
 	}
 
-	if (n > 0)
-	{
-		cout << s << "\"재귀함수가 뭔가요?\"\n";
-		cout << s << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
-		cout << s << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
-		cout << s << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+	cout << s << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+	cout << s << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+	cout << s << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+	
+	jh_say(n - 1, s + "____");
 
-		return jh_say(n - 1, s + "____");
-	}
-	else
-	{
-		cout << s << "라고 답변하였지.\n";
-		if (s != "")
-			return jh_say(n, s.erase(0, 4));
-		else
-			return "";
-	}
+	cout << s << "라고 답변하였지.\n";
 }
 
 int main()
