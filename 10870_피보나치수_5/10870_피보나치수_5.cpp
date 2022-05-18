840dd2c8c622d862558e9482fe0bb0430cb28d52
미니 <kminh940@naver.com>
2022-05-18 22:22:48 +0900

    17478_재귀함수가 뭔가요?

--- 10870_피보나치수_5/10870_피보나치수_5.cpp
+++ 10870_피보나치수_5/10870_피보나치수_5.cpp
@@ -1,36 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-//int fibonacci(int n, int cnt, int fibo[])
 int fibonacci(int n)
 {
 	// Fn = Fn-1 + Fn-2
-	// F[cnt] = F[cnt-1] + F[cnt-2]
 
 	if (n < 2)
 		return n;
 
+	printf("%d\n", n);
+
 	return fibonacci(n - 1) + fibonacci(n - 2);
-
-	/*
-	fibo[0] = 0;
-
-	if (n > 0 && cnt < 1)
-	{
-		cnt++;
-		fibo[cnt] = cnt;
-		return fibonacci(n - 1, cnt, fibo);
-	}
-	else if (n > 0)
-	{
-		cnt++;
-		fibo[cnt] = fibo[cnt - 1] + fibo[cnt - 2];
-		return fibonacci(n - 1, cnt, fibo);
-	}
-	else
-		return fibo[cnt];
-	*/
-	
+		
 }
 
 int main()
@@ -40,8 +21,6 @@ int main()
 	scanf("%d", &n);
 
 	printf("%d", fibonacci(n));
-
-	//printf("%d", fibonacci(n, 0, arr));
 
 	return 0;
 }
