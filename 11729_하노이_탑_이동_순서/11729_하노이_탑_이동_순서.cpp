31e902f2eb3de104a209ac8319fb06997e95e58f
미니 <kminh940@naver.com>
2022-06-06 17:55:02 +0900

    11729 하노이 탑 이동순서

--- 11729_하노이_탑_이동_순서/11729_하노이_탑_이동_순서.cpp
+++ 11729_하노이_탑_이동_순서/11729_하노이_탑_이동_순서.cpp
@@ -1,29 +1,22 @@
-#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <cmath>
-using namespace std;
 
 void hanoi(int n, int a, int b, int c)
 {
 	if (n == 0) return;
-
 	hanoi(n - 1, a, c, b);
-
-	cout << a << " " << c << endl;
-
+	printf("%d %d\n", a, c);
 	hanoi(n - 1, b, a, c);
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
 	int n, k;
-
-	cin >> n;
-
-	k = pow(2, n) - 1;
-
-	cout << k << endl;
-
+	
+	scanf("%d", &n);
+	
+	printf("%d\n", (int)pow(2, n) - 1);
+	
 	hanoi(n, 1, 2, 3);
 }
