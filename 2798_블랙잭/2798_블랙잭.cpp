f78ad6514015b4d2a88ce83bb3776df76fd61bea
미니 <kminh940@naver.com>
2022-06-13 23:54:21 +0900

    2798_블랙잭

--- 2798_블랙잭/2798_블랙잭.cpp
+++ 2798_블랙잭/2798_블랙잭.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int add_card(int* card, int n, int m, int add, int minus)
+int main()
 {
+	int n, m, c, ans;
+	int card[100] = { 0, };
+	char sum[300000] = { 0, };
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> card[i];
+	}
+
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = i + 1; j < n; j++)
 		{
 			for (int k = j + 1; k < n; k++)
 			{
-				int sum = card[i] + card[j] + card[k];
+				c = card[i] + card[j] + card[k];
+			
+				if (m == c) break;
+				else sum[c] = 1;
+			}
+			if (m == c) break;
+		}
+		if (m == c) break;
+	}
 
-				if ((add == m && sum == m || add != m && (sum == add || sum == minus)))
-				{
-					return sum;
-				}
+	if (m == c)
+	{
+		cout << c;
+	}
+	else
+	{
+		if (c < m)
+		{
+			for (int i = m; i > c; i--)
+			{
+				if (sum[i] == 1) cout << i;
+			}
+		}
+		else
+		{
+			for (int i = c; i < m; i++)
+			{
+				if (sum[i] == 1) cout << i;
 			}
 		}
 	}
-
-	return add_card(card, n, m, add + 1, minus - 1);
-}
-
-int main()
-{
-	int n, m;
-	int card[100] = { 0, };
-
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++)
-	{
-		cin >> card[i];
-	}
-
-	cout << add_card(card, n, m, m, m);
 
 	return 0;
 }
